711817e69f86786a4b670137987c5341008def55
Babar Ali <babaraliengineer32@gmail.com>
2024-02-22 14:04:35 +0500

    5 th step is added , division of two numbers

--- src/one.java
+++ src/one.java
@@ -1,12 +1,15 @@
 public class one {
     public static void main(String[] args) {
         //lets make a calculator step one is addition of two number, this is 5 line code only
-        int a=3;
+        int a=8;
         int b=4;
         int sum=a+b;
         System.out.println(sum);
         // add multiplication step
         int multiple=a*b;
         System.out.println(multiple);
+        // 5th step division of two numbers
+        int divi=a/b;
+        System.out.println(divi);
     }
 }
