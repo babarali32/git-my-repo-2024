b2c0afbb98e59bb3f31be717bfe1b4aae825d600
Babar Ali <babaraliengineer32@gmail.com>
2024-02-22 10:37:27 +0500

    addition of two numbers, making calculator

---
+++ src/one.java
@@ -0,0 +1,9 @@
+public class one {
+    public static void main(String[] args) {
+        //lets make a calculator step one is addition of two number, this is 5 line code only
+        int a=3;
+        int b=4;
+        int sum=a+b;
+        System.out.println(sum);
+    }
+}
